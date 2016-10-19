c88ebb763421c32de4cd36002d46fb8d2e0f38a0
betonniere <19312150+betonniere@users.noreply.github.com>
2016-10-19 23:31:59 +0200

    Scratchpad

--- web/scripts/cricket.py
+++ web/scripts/cricket.py
@@ -1,29 +1,88 @@
 from browser import console
 
 #----------------------------------
-class Player :
+class ScoreBoard:
     # ----
-    def __init__ (self, name):
-        self.name = name
+    def __init__ (self):
+        self.locks = {15:3, 16:3, 17:3, 18:3, 19:3, 20:3}
+
+    # ----
+    def removeLock (self, number, power):
+        score = 0
+        if number in self.locks:
+            for i in range (0, power):
+                if self.locks[number] > 0:
+                    self.locks[number] -= 1
+                else:
+                    score += 1
+
+        return score
+
+    # ----
+    def isLockedFor (self, number):
+        return self.locks[number] < 3
+
 
 #----------------------------------
-class Cricket :
+class Player:
+    # ----
+    def __init__ (self, name):
+        self.name       = name
+        self.darts_used = 0
+        self.score      = 0
+        self.scoreboard = ScoreBoard ()
+
+    # ----
+    def resetVolley (self):
+        self.darts_used = 0
+
+    # ----
+    def volleyIsDone (self):
+        return self.darts_used >= 3
+
+    # ----
+    def onHit (self, number, power, competitors):
+        if not self.volleyIsDone ():
+            scored = self.scoreboard.removeLock (number, power)
+            self.darts_used += 1
+
+            if scored > 0:
+                for c in competitors:
+                    if c.scoreboard.isLockedFor (number):
+                        self.score += number*scored
+                        break
+
+    # ----
+    def draw (self, ctx):
+        ctx.font         = 'bold 3pt sans-serif'
+        ctx.fillStyle    = 'green'
+        ctx.textAlign    = 'left'
+        ctx.textBaseline = 'middle'
+
+        ctx.fillText (self.name + ' ' + str (self.score),
+                      0,
+                      0)
+
+
+#----------------------------------
+class Cricket:
     # ----
     def __init__ (self):
         self.players = []
-        self.player_count = 4
-        for i in range (1, self.player_count+1):
-            player = Player ('Player' + str (i))
+        self.player_count = 2
+        for p in range (1, self.player_count+1):
+            player = Player ('Player' + str (p))
             self.addPlayer (player)
 
-        self.current_player = 0
-        self.hit_count      = 0
+        self.current  = None
+        self.iterator = None
+        self.startRound ()
 
     # ----
-    def onHit (self, point, power):
-        console.log ("===> " + self.players[self.current_player].name)
-        self.hit_count += 1
-        if self.hit_count == 3:
+    def onHit (self, number, power):
+        self.current.onHit (number, power, self.players)
+
+        if self.current.volleyIsDone ():
             self.togglePlayer ()
 
     # ----
@@ -31,7 +90,20 @@ class Cricket :
         self.players.append (player)
 
     # ----
+    def startRound (self):
+        self.iterator = iter (self.players)
+        self.current  = next (self.iterator)
+
+    # ----
     def togglePlayer (self):
-        self.hit_count = 0
-        self.current_player += 1
-        self.current_player = self.current_player % self.player_count
+        try:
+            self.current.resetVolley ()
+            self.current = next (self.iterator)
+        except StopIteration:
+            self.startRound ()
+
+    # ----
+    def draw (self, ctx):
+        for p in self.players:
+            ctx.translate (0, 10)
+            p.draw (ctx)
--- web/scripts/dartboard.py
+++ web/scripts/dartboard.py
@@ -1,4 +1,5 @@
 from browser import document
+from browser import html
 from browser import alert
 from browser import console
 from browser import websocket
@@ -18,10 +19,16 @@ class Dartboard:
 
     # ----
     def __init__ (self, game):
-        self.canvas = document['dartboard_canvas']
-        self.ctx    = self.canvas.getContext ('2d')
         self.socket = None
         self.game   = game
+
+        self.canvas = document['dartboard_canvas']
+        self.ctx    = self.canvas.getContext ('2d')
+
+        self.scratchpad  = html.CANVAS (width = self.canvas.width, height = self.canvas.height)
+        self.scratch_ctx = self.scratchpad.getContext ('2d')
+
+        self.ctx.drawImage (self.scratchpad, 0, 0)
 
     # ----
     def onSocketOpen (self, evt):
@@ -32,14 +39,14 @@ class Dartboard:
         if msg.data == "HELLO":
             pass
         elif msg.data == "GOODBYE":
-            alert (msg.data);
+            alert (msg.data)
         else:
-            param = msg.data.split ('.');
-            point = int (param[0])
-            power = int (param[1]);
+            param  = msg.data.split ('.')
+            number = int (param[0])
+            power  = int (param[1])
 
-            self.game.onHit (point, power)
-            self.draw (point, power)
+            self.game.onHit (number, power)
+            self.draw (number, power)
 
     # ----
     def onSocketClose (self, evt):
@@ -57,56 +64,67 @@ class Dartboard:
         self.socket.bind ('close',   self.onSocketClose)
 
     # ----
-    def draw (self, focus_point, focus_power):
+    def draw (self, focus_number, focus_power):
+        self.scratch_ctx.fillStyle = 'white'
+        self.scratch_ctx.fillRect (0, 0, self.canvas.width, self.canvas.height)
+
         if (self.canvas.width <= self.canvas.height):
             size = self.canvas.width
         else:
             size = self.canvas.height
 
-        self.ctx.save ()
-        self.ctx.scale     (size/100, size/100)
-        self.ctx.translate (100/2, 100/2)
+        self.scratch_ctx.save ()
+        self.scratch_ctx.scale     (size/100, size/100)
+        self.scratch_ctx.translate (100/2, 100/2)
 
         # Background
-        self.ctx.arc (0, 0, 50, 0, 2*pi, False)
-        self.ctx.fillStyle = 'black'
-        self.ctx.fill   ()
+        self.scratch_ctx.arc (0, 0, 50, 0, 2*pi, False)
+        self.scratch_ctx.fillStyle = 'black'
+        self.scratch_ctx.fill   ()
 
-        # Points
-        self.ctx.font         = 'bold 3pt sans-serif'
-        self.ctx.fillStyle    = 'White'
-        self.ctx.textAlign    = 'center'
-        self.ctx.textBaseline = 'middle'
+        # Numbers
+        self.scratch_ctx.font         = 'bold 3pt sans-serif'
+        self.scratch_ctx.fillStyle    = 'White'
+        self.scratch_ctx.textAlign    = 'center'
+        self.scratch_ctx.textBaseline = 'middle'
 
         for i in range (1, 21):
-            self.ctx.fillText (str (i),
-                               46*cos((i * 2*pi/20) - pi/2),
-                               46*sin((i * 2*pi/20) - pi/2))
+            self.scratch_ctx.fillText (str (i),
+                                       46*cos((i * 2*pi/20) - pi/2),
+                                       46*sin((i * 2*pi/20) - pi/2))
 
         # Sectors
-        self.ctx.save ()
-        self.ctx.rotate (-pi/2 + 2*pi/20)
-        self.ctx.strokeStyle = 'rgb(100,100,100)'
+        self.scratch_ctx.save ()
+        self.scratch_ctx.rotate (-pi/2 + 2*pi/20)
+        self.scratch_ctx.strokeStyle = 'rgb(100,100,100)'
 
         # Big sectors
         for i in range (1, 21):
           sector = BigSector (i+1, Dartboard.safe_area)
-          sector.draw (self.ctx, focus_point, focus_power)
+          sector.draw (self.scratch_ctx, focus_number, focus_power)
 
         # Power sectors
         for power in range (2, 4):
             for i in range (1, 21):
                 sector = SmallSector (i+1, Dartboard.safe_area, power, 3)
-                sector.draw (self.ctx, focus_point, focus_power)
+                sector.draw (self.scratch_ctx, focus_number, focus_power)
 
-        self.ctx.restore ()
+        self.scratch_ctx.restore ()
 
         # Bull's eye
         for power in range (1, 3):
             sector = BullSector (25*power, Dartboard.safe_area, power)
-            sector.draw (self.ctx, focus_point, focus_power)
+            sector.draw (self.scratch_ctx, focus_number, focus_power)
 
-        self.ctx.restore ()
+        self.scratch_ctx.save ()
+        self.scratch_ctx.translate (-50, 55)
+        self.game.draw (self.scratch_ctx)
+        self.scratch_ctx.restore ()
+
+        self.scratch_ctx.restore ()
+
+        self.ctx.drawImage (self.scratchpad, 0, 0)
+
 
 
 #----------------------------------
